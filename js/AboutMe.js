abe6d7cc22f5e777fe4d8f582e28789893a50177
CHuynh97 <chris.huynh17@gmail.com>
2016-07-06 02:24:33 -0400

    Made custom animation, need to work on animating individually

--- js/AboutMe.js
+++ js/AboutMe.js
@@ -1,11 +1,23 @@
 $(document).ready(function() {
-	
-	$(".infoContainer").hide()
-	if ($("div").is(":visible")){
-		$(".infoContainer").fadeIn();
+	function isOnScreen(element) {
+		var topWindow = $(window).scrollTop();
+		var bottomWindow = topWindow + $(window).height();
+		var elementTop = $(element).offset().top;
+		var elementBottom = elementTop + $(element).height();
+
+		return ((elementBottom <= bottomWindow) && (elementTop >= topWindow));
 	}
-		
 	
+
+	if (!isOnScreen(".infoText")) {
+		$(".infoText").css({
+			animation-play-state: "paused";
+		});
+	} else {
+		$(".infoText").css({
+			animation-play-state: "running";
+		});
+	}
 
 });
 
