ac16ccd2c4b07367f00b3ed53f51df3dd1c50f91
CHuynh97 <chris.huynh17@gmail.com>
2016-07-07 02:43:47 -0400

    Changed active nav item in js script, working on fixing layout

--- js/Content.js
+++ js/Content.js
@@ -9,9 +9,40 @@ $(document).ready(function() {
 	}
 	
 
-	setTimeout(function() {
-		$(".infoText").removeClass("startUp");
-	}, 1000);
+	//switches which NavBar item is active
+	function changeActive(id1, id2) {
+		if (!($(id1).hasClass("active"))) {
+			$(id1).addClass("active")
+		}
+		if ($(id2).hasClass("active")) {
+			$(id2).removeClass("active")
+		}
+	}
+	//scroll to next section
+	function scrollToID(id) {
+		//while (!isOnScreen(id)) {
+			window.scrollBy(0, 400);
+		//}
+	}
+	
+	$(window).sc
+	$("#AboutMeNav").addClass("active");
+	$("#ProjectsNav").click(function(){
+		changeActive("#ProjectsNav", "#AboutMeNav");
+		scrollToID("#ProjectsNav")
+	});
+	$("#AboutMeNav").click(function(){
+		changeActive("#AboutMeNav", "#ProjectsNav");
+	});
+
+
+	//setTimeout(function() {
+	//	$(".infoText").AddClass("startUp");
+	//}, 1000);
+
+
 });
+
+
 
 
