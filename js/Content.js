8a9deb08961aca458e016a5fc9d92971a0e30503
CHuynh97 <chris.huynh17@gmail.com>
2016-07-07 21:11:02 -0400

    Biggest jump yet
    Stored all ids that slideLeft in array in content.js,
    WILL MAKE CODE MUCH CLEANER. YAASS!
    Added new nav item for technical skills
    Next steps:
    create bootstrapmodal for contact
    add list of technical skills
    (optional) add progress bar for each skill
    add images for each section
    refine animations

--- js/Content.js
+++ js/Content.js
@@ -1,44 +1,101 @@
+
 $(document).ready(function() {
 	function isOnScreen(element) {
 		var topWindow = $(window).scrollTop();
-		var bottomWindow = topWindow + $(window).height();
+		var bottomWindow = topWindow + ($(window).height())/1.5;
+
 		var elementTop = $(element).offset().top;
 		var elementBottom = elementTop + $(element).height();
 
 		return ((elementBottom <= bottomWindow) && (elementTop >= topWindow));
 	}
+
+	//Array of navbar items
+	var navList = ["#AboutMeNav", "#SkillsNav", "#ProjectsNav"];
+
+	//get all ids with hideOnStart class, store in arrays
+	var allElements = document.getElementsByTagName("*");
+	var animatingInfo = new Array();
+	for (var i = 0; i < allElements.length; i++) {
+		var element = allElements[i];
+		if (element.id) {
+			var nextID = "#"+element.id;
+			if ($(nextID).hasClass("animateInfo")) {
+				animatingInfo.push(nextID);
+			}
+			
+		}
+	}
 	
+	//log ids for testing
+	for (var j = 0; j < animatingInfo.length; j++) {
+		console.log(animatingInfo[j]);
+		console.log(isOnScreen(animatingInfo[j]));
+	}
+	//console.log(allIds.length);
+	
+	
+	//from allIds find and return specified id
+	//function getID(id) {
+
+	//}
 
 	//switches which NavBar item is active
-	function changeActive(id1, id2) {
-		if (!($(id1).hasClass("active"))) {
-			$(id1).addClass("active")
+	function changeActiveNav(id) {
+		var i = 0;
+		while ((i<navList.length) && (!$(navList[i]).hasClass("activeNav"))) {
+			i++;
 		}
-		if ($(id2).hasClass("active")) {
-			$(id2).removeClass("active")
-		}
+		if (navList[i] != id) {
+			$(navList[i]).removeClass("activeNav");
+			$(id).addClass("activeNav");
+		}		
 	}
-	//scroll to next section
-	function scrollToID(id) {
-		//while (!isOnScreen(id)) {
-			window.scrollBy(0, 400);
-		//}
-	}
+
+	//function to scroll to header when nav item clicked
+	//function scrollToId(element){
+	//
+	//}
 	
-	$(window).sc
-	$("#AboutMeNav").addClass("active");
-	$("#ProjectsNav").click(function(){
-		changeActive("#ProjectsNav", "#AboutMeNav");
-		scrollToID("#ProjectsNav")
+	//implement onclick listeners for navbar items
+	$("#AboutMeNav").addClass("activeNav");
+	$("#AboutMeNav").click(function() {
+		changeActiveNav("#AboutMeNav");
 	});
-	$("#AboutMeNav").click(function(){
-		changeActive("#AboutMeNav", "#ProjectsNav");
+	$("#SkillsNav").click(function() {
+		changeActiveNav("#SkillsNav");
 	});
+	$("#ProjectsNav").click(function() {
+		changeActiveNav("#ProjectsNav");
+	});
+	
 
 
-	//setTimeout(function() {
-	//	$(".infoText").AddClass("startUp");
-	//}, 1000);
+	for(var i = 0; i < 4; i++) {
+		var id = animatingInfo[i];
+		if (isOnScreen(id)) {
+			if ($(id).hasClass("hideOnStart")) {
+				$(id).removeClass("hideOnStart");		
+				//$(id).addClass(id);
+			}
+			
+		}
+	}
+	console.log(animatingInfo.length);
+
+	//console.log(isOnScreen("#AboutMeh1"));
+	
+	//$(window).scroll(function(){
+	//	for(var i = 0; i < allIds.length; i++) {
+	//		var id = "#"+allIds[i];
+	//		if (isOnScreen(i)) {
+	//			$(i).removeClass(i);		
+	//			$(i).addClass(i);
+	//		}
+	//	}
+	//});
+
+	
 
 
 });
